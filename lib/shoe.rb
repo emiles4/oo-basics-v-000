042b874e7fbf62ac76dcb2c7b3904135d7ab1b69
Emily Miles <emiles4@gmail.com>
2019-01-03 22:32:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Book
-  attr_accessor :brand, :color, :size, :condition, :material, 
-  attr_reader :title
- 
+  attr_accessor :brand, :color, :size, :condition, :material,
+  attr_reader :condition
+
   def initialize(brand)
     @brand = brand
   end
- 
+
   def cobble
     puts "Your shoe is as good as new!"
   end
- 
+
 end
