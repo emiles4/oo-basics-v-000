d172c85e2a0944098c3409ba8c0b57b032ab2775
Emily Miles <emiles4@gmail.com>
2019-01-03 22:32:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Book
+  attr_accessor :brand, :color, :size, :condition, :material, 
+  attr_reader :title
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+ 
+end
